99d375633d94bba6c24508aa1f1fc2e78815d26a
mobina74 <mobinarauf8@gmail.com>
2020-10-20 06:33:38 -0700

    first commit

---
+++ VariableAssignment.java
@@ -0,0 +1,17 @@
+ public class VariableAssignment
+ {
+   public static void main(String[] args)
+   {
+     int x = 3;
+     int y = 2;
+     System.out.println(x);
+     System.out.println(y);
+     x = y;
+     System.out.println(x);
+     System.out.println(y);
+     y = 5;
+     System.out.println(x);
+     System.out.println(y);
+   }
+ }
+ 
